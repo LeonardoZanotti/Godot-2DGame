f2c805ac39415f71e839b12efc5485062031bbd9
LeonardoZanotti <leonardojosezanotti@gmail.com>
2021-09-11 16:44:37 -0300

    stomp impulse

--- 2DGame/src/Scripts/Enemy.gd
+++ 2DGame/src/Scripts/Enemy.gd
@@ -4,6 +4,12 @@ func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -_speed.x
 
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += _gravity * delta
 	if is_on_wall():
