5e40f63d12c75a51861ae60b61b50cee035e0f26
LeonardoZanotti <leonardojosezanotti@gmail.com>
2021-09-11 15:21:17 -0300

    player movement

--- 2DGame/src/Scripts/Actor.gd
+++ 2DGame/src/Scripts/Actor.gd
@@ -2,10 +2,7 @@ extends KinematicBody2D
 
 class_name Actor
 
+const FLOOR_NORMAL := Vector2.UP
 var velocity := Vector2.ZERO
-export var gravity: float = 3000.0
-export var speed := Vector2(300.0, 1000.0)
-
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
+export var gravity: float = 4000.0
+export var speed := Vector2(800.0, 1000.0)
